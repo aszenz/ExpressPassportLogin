e3263157ab6a3d6b734738a1dfa02d5ca6ed2369
Asrar <asrar.nehvi@gmail.com>
2018-12-05 22:01:57 +0530

    removed api keys

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
+// add your own api keys and secrets
+// ensure that the specified callback are used
 module.exports = {
-    "facebook_api_key": "293938227905424",
-    "facebook_api_secret": "b0022857a5dae4812ef749b8c26ab772",
-    "google_api_key": "33890693080-u9onrdhkk2l7h5pmqj7vl54iqmn4pjrd.apps.googleusercontent.com",
-    "google_api_secret": "t8SDu7mQfM7YsvcMBr1BYGN_",
+    "facebook_api_key": "",
+    "facebook_api_secret": "",
+    "google_api_key": "",
+    "google_api_secret": "",
     "facebook_callback_url": "http://localhost:3000/auth/facebook/callback",
     "google_callback_url": "http://localhost:3000/auth/google/callback"
 }
